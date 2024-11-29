2a931f2560a2ed08c6d13f39e9b2762dd4ee2a14
Hariom Bansiwal <144981798+hariombansiwalgthb@users.noreply.github.com>
2024-11-29 14:17:57 +0530

    Update clock conanfile.py

--- Encora_Modern_C++_Cmake_Conan_Assignment/clock/conanfile.py
+++ Encora_Modern_C++_Cmake_Conan_Assignment/clock/conanfile.py
@@ -1,8 +1,26 @@
-####################################
-# DO WRITE CODE BELOW THIS POINT
-####################################
-
 from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
-class clockRecipe(ConanFile):
-    name = "clock"
+class ClockLibRecipe(ConanFile):
+    name = "clock_lib"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "graph_lib/1.0"  # Dependency on graph library
+    exports_sources = "include/*", "src/*", "CMakeLists.txt"
+    generators = "CMakeToolchain", "CMakeDeps"
+    
+#    def layout(self):
+#        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["clock_lib"]
+
