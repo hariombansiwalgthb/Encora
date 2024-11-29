0bb14d289e8791185ffd60e30f8a7bf6d672b3bd
Hariom Bansiwal <144981798+hariombansiwalgthb@users.noreply.github.com>
2024-11-29 14:14:49 +0530

    Update graph conanfile.py

--- Encora_Modern_C++_Cmake_Conan_Assignment/graph/conanfile.py
+++ Encora_Modern_C++_Cmake_Conan_Assignment/graph/conanfile.py
@@ -1,8 +1,22 @@
-####################################
-# DO WRITE CODE BELOW THIS POINT
-####################################
-
 from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
-class graphRecipe(ConanFile):
-    name = "graph"
+class GraphLibRecipe(ConanFile):
+    name = "graph_lib"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    exports_sources = "include/*", "src/*", "CMakeLists.txt"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["graph_lib"]
+
