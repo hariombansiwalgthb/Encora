197e8cf9c6b255267a09fc44d695c80f91e37849
Hariom Bansiwal <144981798+hariombansiwalgthb@users.noreply.github.com>
2024-11-29 14:19:14 +0530

    Update clock.h

--- Encora_Modern_C++_Cmake_Conan_Assignment/clock/include/clock.h
+++ Encora_Modern_C++_Cmake_Conan_Assignment/clock/include/clock.h
@@ -4,25 +4,13 @@
 
 #pragma once
 
-#include <graph.h>
+#include <chrono>
+#include "graph.h"
 
-static const int s_blockQuantity = 1000;
-static const int s_attributesPerBlock = 250;
-
-static const int s_distinctAttributes = 300;
-static const int s_distinctBlocks = 10;
-static std::string s_blockBaseName("blockNameBase_");
-static std::string s_attrBaseName("attributeBase_");
-
-static std::vector<std::string> s_attrs;
-static std::vector<std::string> s_blockNames;
+//moved constants and globals to constants.h
 
 void createBlockNames();
-
 void createAttrNames();
-
 std::chrono::milliseconds Measure_AddBlocks(Graph& graph);
-
-std::chrono::milliseconds Measure_FindAttributesByBlock(Graph& graph);
-
-std::chrono::milliseconds Measure_FindBlocksByAttribute(Graph& graph);
+std::chrono::milliseconds Measure_FindAttributesByBlock(const Graph& graph);
+std::chrono::milliseconds Measure_FindBlocksByAttribute(const Graph& graph);
